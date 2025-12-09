8625461ae1b2866d6491ed09a87688c7c4227fbc
BrennoDornelasMF <bdmf@cesar.school>
2025-12-09 18:28:02 -0300

    save 1.0

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const nome = document.getElementById('nome');
-const mensagem = document.getElementById('mensagem');
-const telefone = '5587981361258';
+function enviarWhats(event){
+    event.preventDefault()
 
-const texto = `Olá! Me chamo ${nome}, ${mensagem}`;
-const msgFormatada = encodeURIComponent(texto)
 
-const url = `https://whatsa.me/${telefone}/?t=${msgFormatada}`
+    const nome = document.getElementById('nome').value;
+    const mensagem = document.getElementById('mensagem').value;
+    const telefone = '5587981361258';
+    
+    const texto = `Olá! Me chamo ${nome}, ${mensagem}`;
+    const msgFormatada = encodeURIComponent(texto);
+    
+    const url = `https://whatsa.me/${telefone}/?t=${msgFormatada}`;
+ 
+    window.open(url, '_blank');
 
-window.open(url, '_blank')
+}
+
